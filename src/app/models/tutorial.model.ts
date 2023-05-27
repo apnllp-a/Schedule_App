fceeb9098af8e8106fe6e9942b81e16a48a188df
apnllp-a <gravity_fz@hotmail.com>
2023-05-27 13:16:41 +0700

    Update MongoDB

---
+++ src/app/models/tutorial.model.ts
@@ -0,0 +1,21 @@
+export class Tutorial {
+  [x: string]: any;
+  id?: any;
+  username?: String;
+  password?: String;
+  firstname?: String;
+  lastname?: String;
+  email?:String;
+  departmentDetail?: {
+    role?: String,
+    salary?: String,
+    department?: String,
+  };
+  status?: {
+    role?: String,
+    active?: boolean,
+  }
+  position?:String;
+  createdAt?: Date;
+  published?: boolean;
+}
