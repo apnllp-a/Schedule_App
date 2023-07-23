185a407f1c470f91d1274d79f65f84e8a05212ad
apnllp-a <gravity_fz@hotmail.com>
2023-07-23 23:06:10 +0700

    Update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'schedule-app',
+  appId: 'com.schedule.schApp',
+  appName: 'schedule app',
   webDir: 'www',
   bundledWebRuntime: false
 };
